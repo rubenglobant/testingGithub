5a57c6fe34be56a08c73800f9153ba178c06660c
Ruben Garcia <ruben.garcia@CO-IT01705.local>
2017-08-30 05:38:49 -0500

    fourth commit

---
+++ Testing my git commands/TestingClass.swift
@@ -0,0 +1,27 @@
+//
+//  TestingClass.swift
+//  Testing my git commands
+//
+//  Created by Ruben Garcia on 8/30/17.
+//  Copyright © 2017 Ruben Garcia. All rights reserved.
+//
+
+import UIKit
+
+class TestingClass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        print("Is just to make tests")
+    
+    
+    }
+
+
+    
+    
+    
+
+}
