9812dae333e20e5be123b361ee02f5a39d888d13
Ruben Garcia <ruben.garcia@CO-IT01705.local>
2017-08-30 04:57:06 -0500

    Project started with a simple print

---
+++ Testing my git commands/Testing my git commands/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Testing my git commands
+//
+//  Created by Ruben Garcia on 8/30/17.
+//  Copyright © 2017 Ruben Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        print("Hello world")
+    
+    
+    
+    }
+
+
+    
+    
+
+}
+
